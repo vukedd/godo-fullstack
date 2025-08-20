ec7346cf5a9c5b664139ea00f31587d611867299
vukedd <vukedd@gmail.com>
2025-08-20 17:43:31 +0200

    Users authentication can now be done on the frontend, log out mechanism, header UI changes

--- GoDo-Frontend/src/app/services/auth/auth.service.ts
+++ GoDo-Frontend/src/app/services/auth/auth.service.ts
@@ -2,15 +2,73 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment.development';
-import { AccountRegistrationRequest } from '../../models/auth/accountRegistrationRequest';
+import { AccountRegistrationRequest } from '../../models/auth/requests/accountRegistrationRequest';
+import { LoginRequest } from '../../models/auth/requests/loginRequest';
+import { TokenDto } from '../../models/auth/tokenDto';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  public sendRegistrationRequest(request: AccountRegistrationRequest): Observable<any> {
-    return this.http.post(environment.apiUrl + "/accountRequest", request);
+  public sendRegistrationRequest(
+    request: AccountRegistrationRequest
+  ): Observable<any> {
+    return this.http.post(environment.apiUrl + '/auth/register', request);
+  }
+
+  public sendLoginRequest(request: LoginRequest): Observable<any> {
+    return this.http.post(environment.apiUrl + '/auth/login', request);
+  }
+
+  public handleLogin(token: TokenDto) {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      localStorage.setItem('accessToken', token.accessToken);
+    }
+  }
+
+  setAccessToken(token: string) {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      localStorage.setItem('accessToken', token);
+    }
+  }
+
+  getAccessToken(): string | null {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      return localStorage.getItem('accessToken');
+    }
+    return null;
+  }
+
+  isLoggedIn() {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      if (
+        localStorage.getItem('accessToken') != null //&&
+        // localStorage.getItem('refreshToken') != null
+      ) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  logout() {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      localStorage.clear();
+    }
+  }
+
+  getUsername() {
+    let token: string | null = this.getAccessToken();
+    
+    if (token == null)
+      return "??????";
+
+    let decodedToken = jwtDecode(token);
+
+    return decodedToken.sub;
   }
 }
